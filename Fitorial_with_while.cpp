e944aa22ebd10f78e1fb35d6a4e9e2b660bd3efa
Muhammad Taha <wwwtahataha343@gmail.com>
2025-02-12 13:59:59 +0500

    Add files via upload

---
+++ Fitorial_with_while.cpp
@@ -0,0 +1,17 @@
+// THIS IS WITH WHILE
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b=1,i;
+cout<<"enter your value that you to convert itinto the fictorial =";
+cin>>a;
+i=a;
+while(i>0){
+b=b*i;
+	i--;
+}
+cout<<"the fictorial of	"<<a<<"!"<<"is="<<b;
+
+
+}
